4434be83032873241384eb7ccb8c515f640cdb78
tbushman <tbushman@pu.bli.sh>
2020-04-27 14:47:32 -0600

    update react-scripts-ts to react-scripts

--- src/__tests__/index.test.tsx
+++ src/__tests__/index.test.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import * as App from '../src';
-import expect from 'expect';
+import App from '../component/app';
+// import expect from 'expect';
 import * as path from 'path';
-import nock from 'nock';
+// import nock from 'nock';
 import SyncGoogleSheetDialog from '../src/component/content/SyncGoogleSheetDialog';
 // import MockSyncGoogleSheetDialog from './fixtures/SyncGoogleSheetDialog';
-import Lll from '../src';
-import { envConfig } from '../src/component/config';
-const nockBack = nock.back;
-nockBack.fixtures = path.join(__dirname, '.', '__nock-fixtures__');
+// import Lll from '../src';
+import { envConfig } from '../config';
+// const nockBack = nock.back;
+// nockBack.fixtures = path.join(__dirname, '.', '__nock-fixtures__');
 const recording = envConfig.recordEnv;
-nockBack.setMode('record');
+// nockBack.setMode('record');
 describe('SyncGoogleSheetDialog API calls', async () => {
   // let googleSheets
   beforeAll(async() => {
